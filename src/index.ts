85f85ba85c732b6259fa1d7543003c1a6bfe1787
吕立奇 <liqi.lv@alsi.cn>
2023-05-29 09:49:17 +0800

    feat: 修改package

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 function flat(obj: {[index: string]: any}) {
-  var newObj: {[index: string]: any} = {}
+  const newObj: {[index: string]: any} = {};
   function format(o, pre) {
-      for (let key in o) {
-          if (typeof o[key] === 'object') {
-              if (!pre) {
-                  format(o[key], key)
-              }else {
-                  if (Array.isArray(o)) {
-                      format(o[key], pre + '[' + key + ']')
-                  }else {
-                      format(o[key], pre + '.' + key)
-                  }
-              }
-          }else {
-              if (!pre) {
-                  newObj[key] = o[key]
-              }else {
-                  if (Array.isArray(o)) {
-                      newObj[pre + '[' + key + ']'] = o[key]
-                  }else {
-                      newObj[pre + '.' + key] = o[key]
-                  }
-              }
+    for (const key in o) {
+      if (typeof o[key] === 'object') {
+        if (!pre) {
+          format(o[key], key);
+        } else {
+          if (Array.isArray(o)) {
+            format(o[key], pre + '[' + key + ']');
+          } else {
+            format(o[key], pre + '.' + key);
           }
+        }
+      } else {
+        if (!pre) {
+          newObj[key] = o[key];
+        } else {
+          if (Array.isArray(o)) {
+            newObj[pre + '[' + key + ']'] = o[key];
+          } else {
+            newObj[pre + '.' + key] = o[key];
+          }
+        }
       }
+    }
   }
-  format(obj, null)
-  return newObj
+  format(obj, null);
+  return newObj;
 }
 export default flat;
