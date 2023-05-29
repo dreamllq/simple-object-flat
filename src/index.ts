68c6c1d3cfa41e90f793fa4b92e91a02b707f367
吕立奇 <liqi.lv@alsi.cn>
2023-05-29 09:45:11 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-export { default as Hello } from './hello.vue';
-export { default as World } from './world.jsx';
-
+function flat(obj: {[index: string]: any}) {
+  var newObj: {[index: string]: any} = {}
+  function format(o, pre) {
+      for (let key in o) {
+          if (typeof o[key] === 'object') {
+              if (!pre) {
+                  format(o[key], key)
+              }else {
+                  if (Array.isArray(o)) {
+                      format(o[key], pre + '[' + key + ']')
+                  }else {
+                      format(o[key], pre + '.' + key)
+                  }
+              }
+          }else {
+              if (!pre) {
+                  newObj[key] = o[key]
+              }else {
+                  if (Array.isArray(o)) {
+                      newObj[pre + '[' + key + ']'] = o[key]
+                  }else {
+                      newObj[pre + '.' + key] = o[key]
+                  }
+              }
+          }
+      }
+  }
+  format(obj, null)
+  return newObj
+}
+export default flat;
